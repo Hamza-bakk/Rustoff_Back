bbaf2c89e2423e8088b68da040d7edfc9421971a
Hamza <bakkar.hamza92@gmail.com>
2023-12-05 14:52:37 +0100

    Show items in "Boutiques" with seed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,7 @@ class ItemsController < ApplicationController
   # GET /items or /items.json
   def index
     @items = Item.all
+    render json: @items
   end
   
   # GET /items/1 or /items/1.json
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,38 @@
-Property.destroy_all
-User.destroy_all
+# db/seeds.rb
 
-10.times do
-  User.create!(
-    email: Faker::Internet.unique.email,
-    password: "123456")
+# Clear existing records
+Item.destroy_all
+
+# Seed data for items
+items_data = [
+  {
+    title: "Item 1",
+    description: "Description for Item 1",
+    price: 19.99,
+    image_url: "image1.png",
+    alt: "Image 1 Alt Text",
+    category: "Animation",
+    disabled: false
+  },
+  {
+    title: "Item 2",
+    description: "Description for Item 2",
+    price: 29.99,
+    image_url: "image2.png",
+    alt: "Image 2 Alt Text",
+    category: '3D',
+    disabled: false
+  }
+]
+
+# Create items with validations
+items_data.each do |item_data|
+  item = Item.new(item_data)
+  if item.save
+    puts "Item '#{item.title}' created successfully!"
+  else
+    puts "Error creating item '#{item.title}': #{item.errors.full_messages.join(', ')}"
+  end
 end
 
-10.times do
-  Property.create!(
-    title: Faker::Book.title,
-    price: rand(100000...1000000000),
-    description: Faker::Quotes::Shakespeare.king_richard_iii_quote,
-    user_id: rand(User.first.id..User.last.id)
-  )
-end
+puts "Seed data for items created successfully!"
