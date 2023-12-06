e0d25beb955b2c6dbbba5fd6bc7bb63e29e67bb1
Yunjidev <florian.vancamp@gmail.com>
2023-12-06 11:32:14 +0100

    ajout img seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ items_data = [
     title: "Item 1",
     description: "Description for Item 1",
     price: 19.99,
-    image_url: "image1.png",
+    image_url: "https://imgs.search.brave.com/opW2mfM4WR2Q90zc9PDSY0rKCL08WJrBOrGR9sJs97I/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTE2/MDc5MTc2Ny9mci9w/aG90by9jaGV2YWwt/cmlhbnQuanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPUItWjdH/bzVqaXZyVFNOS0JR/clJJdG9rU1k2UV9w/U3NKQ0N5VnpmMFdP/a1U9",
     alt: "Image 1 Alt Text",
     category: "Animation",
     disabled: false
@@ -18,7 +18,7 @@ items_data = [
     title: "Item 2",
     description: "Description for Item 2",
     price: 29.99,
-    image_url: "image2.png",
+    image_url: "https://imgs.search.brave.com/I26EV9Z3LcNGfMwRWh5gzszGWqJcpUqv5a1IInn59is/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNDcz/MDEyNjYwL2ZyL3Bo/b3RvLyVDMyVBOWN1/cmV1aWwtZ3Jpcy1i/JUMzJUEyaWxsZXIu/anBnP3M9NjEyeDYx/MiZ3PTAmaz0yMCZj/PThSM2JLUUNtb0Z2/a3RRNXEwUXNyeGNS/MjBITEpOREZLdXhz/SUJMb0R6ZVU9",
     alt: "Image 2 Alt Text",
     category: '3D',
     disabled: false
