c19c897a615d54060c21a75b5d67d380407978bb
Yunjidev <florian.vancamp@gmail.com>
2023-12-07 14:03:17 +0100

    Ajout devis

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,12 @@ Rails.application.routes.draw do
              }
   get '/member-data', to: 'members#show'
 
-  
+  resources :quotes, only: [:index, :new, :create, :show, :destroy] do
+    member do
+      post :mark, action: :mark
+      post :reprocess
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
