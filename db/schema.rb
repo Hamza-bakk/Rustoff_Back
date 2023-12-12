dc7a305a37984476ed0817ccb52d2d3a8b3cee68
Yunjidev <florian.vancamp@gmail.com>
2023-12-12 18:02:09 +0100

    resolvequotesbug

--- db/schema.rb
+++ db/schema.rb
@@ -133,7 +133,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_11_152528) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
-    t.string "token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
