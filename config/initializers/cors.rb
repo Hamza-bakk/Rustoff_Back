9f36b5855f4f89e8da9b92d3a613530b695fa802
Hamza <bakkar.hamza92@gmail.com>
2023-12-13 15:13:42 +0100

    correction cors url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Rails.env.development? ? 'http://localhost:3000/' : 'https://rustoffback-d3677a58098b.herokuapp.com/'
+    origins 'https://rustoffback-d3677a58098b.herokuapp.com'
     
     resource '*',
       headers: :any,
